ff1a85a01766c4f00af24993a71ae0c34c19830f
billhungau <billhungau@gmail.com>
2026-09-13 07:36:44 -0700

    Load final Veterans TMS responsive polish

---
+++ src/app/(frontend)/veterans-tms-treatment/layout.tsx
@@ -0,0 +1,6 @@
+import type { ReactNode } from "react";
+import "./veterans-tms-polish.css";
+
+export default function VeteransTmsTreatmentLayout({ children }: { children: ReactNode }) {
+  return children;
+}
